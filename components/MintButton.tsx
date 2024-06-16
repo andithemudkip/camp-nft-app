2a84175ace9b8af186a25011c662a3722d69e607
Andrei Cracanau <andithemudkip@gmail.com>
2024-06-17 01:16:30 +0300

    feat: complete mint modal UI

--- components/MintButton.tsx
+++ components/MintButton.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import { FileUploader } from "react-drag-drop-files";
+const fileTypes = ["JPG", "JPEG", "PNG", "GIF"];
+
 const MintButton = () => {
+  const [file, setFile] = useState(null);
+  const handleChange = (file: any) => {
+    setFile(file);
+  };
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
@@ -17,27 +24,48 @@ const MintButton = () => {
           <Dialog.Description className="DialogDescription">
             Fill in the details below to create your collectible.
           </Dialog.Description>
-          <fieldset className="Fieldset">
-            <label className="Label" htmlFor="name">
-              Name
-            </label>
-            <input className="Input" id="name" placeholder="My NFT" />
-          </fieldset>
-          <fieldset className="Fieldset">
-            <label className="Label">Description</label>
-            <textarea
-              className="Textarea"
-              id="description"
-              placeholder="This is an NFT."
-            />
-          </fieldset>
-          <div
-            style={{
-              display: "flex",
-              marginTop: 25,
-              justifyContent: "flex-end",
-            }}
-          >
+          <div className="flex flex-row items-end justify-between gap-4">
+            <div className="flex-1">
+              <fieldset className="Fieldset">
+                <label className="Label" htmlFor="name" aria-required>
+                  Name
+                </label>
+                <input className="Input" id="name" placeholder="My NFT" />
+              </fieldset>
+              <fieldset className="Fieldset">
+                <label className="Label">Description</label>
+                <textarea
+                  className="Textarea"
+                  id="description"
+                  placeholder="This is an NFT."
+                />
+              </fieldset>
+              <FileUploader
+                handleChange={handleChange}
+                name="file"
+                types={fileTypes}
+                classes="drop-area"
+                required
+              />
+            </div>
+            <div className="self-center">
+              {file ? (
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt="preview"
+                  className="h-40 w-40 object-cover rounded"
+                />
+              ) : (
+                <div className="h-40 w-40 bg-zinc-900 rounded">
+                  <p className="text-zinc-400 text-center pt-16">
+                    Image preview
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="w-full flex justify-end items-end mt-3">
             <Dialog.Close asChild>
               <button className="Button green">Mint</button>
             </Dialog.Close>
